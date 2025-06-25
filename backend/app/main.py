f80aa3437242b1924907230fd1698e7df7dc1512
Carmela Guida Aquilante <carmela.guidaaquilante@doing.com>
2025-06-25 17:35:54 +0200

    updates main.py by using env variables

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1 import endpoints
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI(title="API Sovraffollamento Abitativo")
+
+origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[origin.strip() for origin in origins if origin],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(endpoints.router, prefix="/api/v1")
