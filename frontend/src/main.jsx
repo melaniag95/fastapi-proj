ab446fe88b62e8e7f13be04e6ed4820bbbeb6546
Carmela Guida Aquilante <carmela.guidaaquilante@doing.com>
2025-06-26 13:28:29 +0200

    removes unwanted comment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n'; // usa l'istanza già inizializzata
+import i18n from './i18n';
 import './index.css';
 import App from './App.jsx';
 
