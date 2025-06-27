ae12451ad38723d89fe86bee1a0ef39a762958c4
Carmela Guida Aquilante <carmela.guidaaquilante@doing.com>
2025-06-27 14:43:13 +0200

    removes extra spaces

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,8 +6,8 @@ export const fetchData = async (year, region, sortBy, sortOrder) => {
   const params = {};
   if (year) params.year = year;
   if (region) params.region = region;
-  if (sortBy) params.sort_by = sortBy;
-  if (sortOrder) params.sort_order = sortOrder;
+  if (sortBy) params.sort_by = sortBy;
+  if (sortOrder) params.sort_order = sortOrder;
 
   const response = await axios.get(`${API_BASE}/data`, { params });
   return response.data;
